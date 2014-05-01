2fa9dc7439e8b8a56680de28c6824e7d94408b08
laetitia <laetitiavanespen@hotmail.com>
2014-05-01 17:40:21 +0200

    updated routes

--- eindlabo/labox/routes/questions.js
+++ eindlabo/labox/routes/questions.js
@@ -1,3 +1,16 @@
+var mongoose = require( 'mongoose' );
+var Chat     = mongoose.model( 'Chat' );
+var allChats = "";
+
 exports.list = function(req, res){
-  res.render('questions', { title: 'IMD WALL' });
+  //res.render('questions', { title: 'IMD WALL' });
+    Chat.find( function ( err, questions, count ){
+	allChats = questions;
+	console.log(allChats);
+    res.render( 'questions', {
+      title : 'IMD WALL',
+      questions : questions,
+      count : count
+    });
+  });
 };
