b00a8216b2d8f60182c7b88af2df08c8145f2969
laetitia <laetitiavanespen@hotmail.com>
2014-05-06 14:03:58 +0200

    updated all, currently working on css, hence working with local url for facebook login

--- eindlabo/labox/app.js
+++ eindlabo/labox/app.js
@@ -12,6 +12,7 @@ var passport = require('passport'), FacebookStrategy = require('passport-faceboo
 var mongoose = require( 'mongoose' );
 var Chat     = mongoose.model( 'Chat' );
 var ChatCount = mongoose.model('ChatCount');
+var Moderator = mongoose.model('Moderator');
 var allChats = "";
 
 
@@ -27,8 +28,57 @@ var faye = require('faye');
 var app = express();
 
 var facebookUserId = "";
+var userDisplayName = "";
+var userFirstName = "" ;
+var userLastName = "" ;
+var userName = "";
 
 
+var moderatorLoggedIn = false;
+var userLoggedIn = false;
+
+
+
+
+// VOOR PASSPORT FACEBOOK RETURN
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+//callback urls voor lokaal en remote te testen
+var callbackURLLocal = "http://localhost:3002/auth/facebook/callback";
+var callbackURLRemote = "http://polar-hamlet-4269.herokuapp.com/auth/facebook/callback";
+
+passport.use(new FacebookStrategy({
+    clientID: "311747825641524",
+    clientSecret: "88978dea32c610c42185bd102cb5c14a",
+    callbackURL: /*__dirname + "/auth/facebook/callback"*/ callbackURLLocal,
+    profileFields: ['id', 'displayName', 'photos']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    /*User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return done(err, user);
+    });*/
+   
+      process.nextTick(function () {
+        
+          facebookUserId = profile.id;
+          userDisplayName = profile.displayName;
+          userFirstName = profile.givenName ;
+          userLastName = profile.familyName  ;
+          userName = profile.name  ;
+          userLoggedIn = true;
+          //console.log(facebookUserId);
+         return done(null, profile);
+       });
+  // NU NOG DE PROFILE GEGEVENS OPHALEN
+  // GUI AANPASSEN
+  }
+));
 
 
 // all environments
@@ -45,20 +95,6 @@ app.use(express.methodOverride());
   app.use(passport.session());
 app.use(app.router);
 
-
-// VOOR PASSPORT FACEBOOK RETURN
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-
-
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -67,38 +103,69 @@ if ('development' == app.get('env')) {
 }
 
 
+
+
+app.get('/auth/facebook',passport.authenticate('facebook') ,
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  });
+
+/*app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/ask');
+  });*/
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/ask',
+  failureRedirect: '/'
+}));
+
+
 // ROUTES DEFINIEREN
 app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/users', user.list);
 //app.get('/ask', askQuestion.ask);
 
 
 
 // BIJ HET NAVIGEREN NAAR /ASK
-app.get('/ask', function(req, res){
+app.get('/ask'/*, ensureAuthenticated*/, function(req, res){
+    if(userLoggedIn)
+    {
+        //var user = req.user;
+        var user = { username : userFirstName, name : userName, displayname :userDisplayName , familyName :userLastName };
 
-    var totalMessagesSent = 0;
-      ChatCount.find({}, function(err, chatCounts){
-                    
-                    //console.log(chatCounts);
-        totalMessagesSent = chatCounts[0].messagesCount;   
-    });
+        var totalMessagesSent = 0;
+          ChatCount.find({}, function(err, chatCounts){
+                        
+                        //console.log(chatCounts);
+            totalMessagesSent = chatCounts[0].messagesCount;   
+        });
 
-    //res.render('questions', { title: 'IMD WALL' });
-      Chat.find( function ( err, questions, count ){
-        allChats = questions;
-        //console.log(allChats);
-          res.render( 'ask', {
-            title : 'IMD WALL',
-            questions : questions,
-            facebookUserID : facebookUserId,
-            count : totalMessagesSent
-          });
-      });
-
-       
-	
+        //res.render('questions', { title: 'IMD WALL' });
+          Chat.find({}).sort('-votes').execFind( function ( err, questions, count ){
+            allChats = questions;
+            //console.log(allChats);
+              res.render( 'ask', {
+                title : 'IMD WALL',
+                questions : questions,
+                facebookUserID : facebookUserId,
+                count : totalMessagesSent,
+                userImg : "https://graph.facebook.com/" + facebookUserId + "/picture?type=large",
+                user: user
+              });
+          }); 
+          }
+      else
+      {
+        res.redirect('/');
+      } 
+  
 });
+
 
 //BIJ HET MAKEN VAN EEN CHAT IN /ASK
 app.post( '/create', askQuestion.create );
@@ -109,31 +176,49 @@ app.get('/questions', allQuestions.list);
 app.get('/moderator', function(req, res){
 
     //res.render('questions', { title: 'IMD WALL' });
-      Chat.find( function ( err, questions, count ){
-        allChats = questions;
-        //console.log(allChats);
-          res.render( 'moderator', {
-            title : 'IMD WALL',
-            questions : questions,
-            facebookUserID : facebookUserId,
-            count : allChats.length
-          });
-      });
+    if(moderatorLoggedIn)
+    {
+
+
+        Chat.find({}).sort('-votes').execFind( function ( err, questions, count ){
+          allChats = questions;
+          //console.log(allChats);
+            res.render( 'moderator', {
+              title : 'IMD WALL',
+              questions : questions,
+              facebookUserID : facebookUserId,
+              count : allChats.length
+            });
+        });
+    }
+    else
+    {
+      res.redirect('/');
+    }
   
 });
 
 //app.delete( '/destroy', moderate.destroy );
 app.post('/destroy/:id', function (req, res){
-  return Chat.find({ id : req.params.id}, function (err, chat) {
-    return Chat.remove(function (err) {
-      if (!err) {
+  var result = false;
+   Chat.find({ id : req.params.id}, function (err, chat) {
+
+     Chat.remove(function (err) {
+      if (!err) 
+      {
         console.log("removed");
-        return res.send('');
-      } else {
+         res.send(JSON.stringify(true));
+
+      } 
+      else 
+      {
         console.log(err);
+        res.send(JSON.stringify(false));
       }
     });
   });
+
+  
 });
 
 app.post('/vote/:id/:vote', function (req, res){
@@ -159,34 +244,42 @@ app.post('/vote/:id/:vote', function (req, res){
 });
 
 
+app.post('/loginMod/:username/:password', function(req, res){
+  var name = req.params.username;
+  var pass = req.params.password;
 
-passport.use(new FacebookStrategy({
-    clientID: "311747825641524",
-    clientSecret: "88978dea32c610c42185bd102cb5c14a",
-    callbackURL: /*__dirname + "/auth/facebook/callback"*/ "https://polar-hamlet-4269.herokuapp.com/auth/facebook/callback",
-    enableProof: false
-  },
-  function(accessToken, refreshToken, profile, done) {
-    /*User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return done(err, user);
-    });*/
-	facebookUserId = profile.id;
-  console.log(facebookUserId);
-	return done(null, profile);
-	// NU NOG DE PROFILE GEGEVENS OPHALEN
-	// DB AAN VASTHANGEN
-	// GUI AANPASSEN
-  }
-));
+  Moderator.find({}, function (err, docs) {
+    console.log(docs);
+    var found = "";
+    for (var i = 0; i < docs.length; i++)
+     {
+       if(docs[i].username == name && docs[i].password == pass)
+       {
+           found = true;
+           moderatorLoggedIn = true;
 
-app.get('/auth/facebook',passport.authenticate('facebook'));
+       }
+       else
+       {
+         found = false;
+         moderatorLoggedIn = false;
+       }
+    };
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/' ,  display: 'touch'}),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/ask');
+      res.send(JSON.stringify(found));
+       
   });
+
+  //console.log(name + ' ' + pass);
+});
+
+app.get('/logout', function(req, res){
+  console.log(req.logout());
+  moderatorLoggedIn = false;
+  userLoggedIn = false;
+  req.logout();
+  res.redirect('/');
+});
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
@@ -200,6 +293,8 @@ server.listen('3002');
 
 // test authentication
 function ensureAuthenticated(req, res, next) {
+  console.log(req.isAuthenticated());
+
 if (req.isAuthenticated()) { return next(); }
-res.redirect('/ask');
+res.redirect('/');
 }
